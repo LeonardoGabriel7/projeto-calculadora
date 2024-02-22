8240664901ebef497d8dd0a3a7ff9d1a1df707a8
Leonardo Gabriel <l3onardo18072006@gmail.com>
2024-02-22 20:02:12 -0300

    html, css, js

---
+++ app.js
@@ -0,0 +1,7 @@
+function insert(num){
+   var numero = document.getElementById('resultado').innerHTML;
+   document.getElementById('resultado').innerHTML = numero + num
+}
+//isso é para quando eu clicar no número exibir ele no p
+
+

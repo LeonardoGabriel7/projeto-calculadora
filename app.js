e76c3153e195fbc227395a18b998a983daab2093
Leonardo Gabriel <l3onardo18072006@gmail.com>
2024-02-22 20:16:55 -0300

    .

--- app.js
+++ app.js
@@ -4,4 +4,23 @@ function insert(num){
 }
 //isso é para quando eu clicar no número exibir ele no p
 
+function clean(){
+    document.getElementById('resultado').innerHTML = ""
+}
 
+function back() {
+    var resultadoElement = document.getElementById('resultado');
+    var resultado = resultadoElement.innerHTML;
+    
+    // Corrigindo o erro de sintaxe aqui
+    resultadoElement.innerHTML = resultado.substring(0, resultado.length - 1);
+}
+
+function calcular(){
+    var resultado = document.getElementById('resultado').innerHTML
+    if(resultado){
+        document.getElementById('resultado').innerHTML = eval(resultado)
+    } else{
+        document.getElementById('resultado')
+    }
+}
